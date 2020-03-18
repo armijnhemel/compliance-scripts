7ca14e2a7ca72eb03c036161ceb12c62ee397aa4
Armijn Hemel <armijn@tjaldur.nl>
2020-03-18 15:18:39 +0100

    fix removing of directory with prebuilts, remove more prebuilts

--- android/removeprebuilts.sh
+++ android/removeprebuilts.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# script to clean up Linux kernel images and other prebuilts from the prebuilt directory
+# script to clean up Linux kernel images and other prebuilts from the prebuilt
+# directory. This assumes that the host machine is a Linux machine (for
+# example: Ubuntu 14.04) on x86 or x86-64. The following components are
+# removed with this script:
+#
+# * Linux kernel images
+# * qemu
+# * all executables compiled for Darwin-x86, Darwin-x86-64, MIPS, MIPS64
 
 
 # set this to your Android directory
 ANDROID_DIR=/path/to/android
 
 # Linux kernel
-
 rm -f ${ANDROID_DIR}/prebuilts/qemu-kernel/arm/2.6/kernel-qemu
 rm -f ${ANDROID_DIR}/prebuilts/qemu-kernel/arm/2.6/kernel-qemu-armv7
 rm -f ${ANDROID_DIR}/prebuilts/qemu-kernel/arm/kernel-qemu
@@ -35,4 +41,26 @@ rm -f ${ANDROID_DIR}/prebuilts/qemu-kernel/x86/kernel-vbox
 rm -f ${ANDROID_DIR}/prebuilts/qemu-kernel/x86/pc-bios/bios.bin
 
 # qemu
-rm -f ${ANDROID_DIR}/prebuilts/android-emulator
+rm -rf ${ANDROID_DIR}/prebuilts/android-emulator
+
+# clang
+rm -rf ${ANDROID_DIR}/prebuilts/clang/darwin-x86
+
+# gcc
+rm -rf ${ANDROID_DIR}/prebuilts/gcc/darwin-x86
+
+# misc
+rm -rf ${ANDROID_DIR}/prebuilts/misc/darwin-x86
+rm -rf ${ANDROID_DIR}/prebuilts/misc/darwin-x86_64
+rm -rf ${ANDROID_DIR}/prebuilts/misc/android-mips
+rm -rf ${ANDROID_DIR}/prebuilts/misc/android-mips64
+
+# python
+rm -rf ${ANDROID_DIR}/prebuilts/python/darwin-x86
+
+# sdk
+rm -rf ${ANDROID_DIR}/prebuilts/sdk/tools/darwin
+
+# tools
+rm -rf ${ANDROID_DIR}/prebuilts/tools/darwin-x86
+rm -rf ${ANDROID_DIR}/prebuilts/tools/darwin-x86_64
