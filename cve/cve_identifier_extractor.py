5f4110c9e457af5b9050d546103f909cadda4bdb
Armijn Hemel <armijn@tjaldur.nl>
2023-12-07 21:42:41 +0100

    rewrite to V5 JSON format

--- cve/cve_identifier_extractor.py
+++ cve/cve_identifier_extractor.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 Armijn Hemel
+# Copyright 2022-2023 Armijn Hemel
 
+import json
 import os
 import pathlib
 import re
 import sys
-import xml.dom
 
 import click
-import defusedxml.minidom
 
 # TODO: this will also partially match URLs
 RE_PATH = re.compile(r'([/\-_a-z0-9\.]+\.[a-z0-9\+]+)', re.IGNORECASE)
@@ -40,144 +39,153 @@ VALID_EXTENSIONS = ['.c', '.cc', '.cpp', '.cxx', '.c++', '.h', '.hh', '.hpp',
 
 @click.command(short_help='process CVE data and extract useful data')
 #@click.option('--config-file', '-c', required=True, help='configuration file', type=click.File('r'))
-@click.option('--cve', '-f', required=True, help='CVE XML file', type=click.File('r'))
-def main(cve):
-    # first some sanity checks, try to parse the contents of the file
-    # TODO: for speed this might be more useful to do as a SAX parser
-    # than a DOM parser.
-    cve_dom = defusedxml.minidom.parse(cve)
+@click.option('--cve-directory', '-d', required=True, help='CVE XML file', type=click.Path(exists=True, path_type=pathlib.Path))
+def main(cve_directory):
+    if not cve_directory.is_dir():
+        raise click.BadParameter('parameter should be a directory')
 
-    for cve_report in cve_dom.getElementsByTagName('item'):
-        cve_number = cve_report.attributes['name'].value
-        description = cve_report.getElementsByTagName('desc')[0].childNodes[0].data
-        if description.startswith('** RESERVED **'):
-            # CVE not yet assigned
-            continue
+    for cve_report in cve_directory.glob('**/*.json'):
+        with open(cve_report, 'r') as cve_json_file:
+            try:
+                cve_json = json.load(cve_json_file)
+            except (json.decoder.JSONDecodeError, NameError):
+                print(f'Invalid JSON in {cve_report}, exiting', file=sys.stderr)
+                sys.exit(1)
+            if not 'cveMetadata' in cve_json:
+                # this is probably the metadata file
+                continue
 
-        # skip a bunch of CVEs
-        if description.startswith('** DISPUTED **'):
-            continue
-        if description.startswith('** REJECT **'):
-            continue
-        if description.startswith('**REJECT**'):
-            continue
-        if description.startswith('** SPLIT **'):
-            continue
-        if description.startswith('** UNVERIFIABLE **'):
-            continue
-        if description.startswith('** UNVERIFIABLE, PRERELEASE **'):
-            continue
+            cve_number = cve_json['cveMetadata']['cveId']
+            cve_result = {'cve': cve_number}
 
-        # CVEs for (older) unsupported software versions
-        if description.startswith('** PRODUCT NOT SUPPORTED WHEN ASSIGNED **'):
-            pass
-        if description.startswith('** UNSUPPORTED WHEN ASSIGNED **'):
-            pass
-        if description.startswith('**VERSION NOT SUPPORTED WHEN ASSIGNED**'):
-            pass
+            # skip a bunch of CVEs. TODO: reserved CVEs
+            if cve_json['cveMetadata']['state'] == 'REJECTED':
+                continue
+            tags = cve_json['containers']['cna'].get('tags', [])
+            if 'disputed' in tags:
+                continue
 
-        cve_result = {'cve': cve_number}
+            skip_cve = False
+            for description in cve_json['containers']['cna'].get('descriptions', []):
+                if description['value'].startswith('** SPLIT **'):
+                    skip_cve = True
+                    break
+                if description['value'].startswith('** UNVERIFIABLE **'):
+                    skip_cve = True
+                    break
+                if description['value'].startswith('** UNVERIFIABLE, PRERELEASE **'):
+                    skip_cve = True
+                    break
 
-        # search for file paths
-        verified_paths = set()
+                # CVEs for (older) unsupported software versions, unsure what to do here
+                if description['value'].startswith('** PRODUCT NOT SUPPORTED WHEN ASSIGNED **'):
+                    pass
+                if description['value'].startswith('** UNSUPPORTED WHEN ASSIGNED **'):
+                    pass
+                if description['value'].startswith('**VERSION NOT SUPPORTED WHEN ASSIGNED**'):
+                    pass
 
-        match_results = RE_PATH.findall(description)
+                # search for file paths
+                verified_paths = set()
 
-        if match_results != []:
-            for match in match_results:
-                match_path = pathlib.Path(match)
-                if match_path.suffix.lower() in VALID_EXTENSIONS:
-                    verified_paths.add(match)
+                match_results = RE_PATH.findall(description['value'])
 
-        cve_result['paths'] = verified_paths
+                if match_results != []:
+                    for match in match_results:
+                        match_path = pathlib.Path(match)
+                        if match_path.suffix.lower() in VALID_EXTENSIONS:
+                            verified_paths.add(match)
+                cve_result['paths'] = verified_paths
 
-        # search for function calls
-        functions = set()
+                # search for function calls
+                functions = set()
 
-        match_results = FUNCTION_CALLS.findall(description)
+                match_results = FUNCTION_CALLS.findall(description['value'])
 
-        if match_results != []:
-            for match in match_results:
-                functions.add(match)
+                if match_results != []:
+                    for match in match_results:
+                        functions.add(match)
 
-        cve_result['functions'] = functions
+                cve_result['functions'] = functions
 
-        # search for Android IDs
-        android_ids = set()
+                # search for Android IDs
+                android_ids = set()
 
-        match_results = ANDROID_ID.findall(description)
-        if match_results == []:
-            match_results = ANDROID_ID2.findall(description)
+                match_results = ANDROID_ID.findall(description['value'])
+                if match_results == []:
+                    match_results = ANDROID_ID2.findall(description['value'])
 
-        if match_results != []:
-            for match in match_results:
-                android_ids.add(match)
+                if match_results != []:
+                    for match in match_results:
+                        android_ids.add(match)
 
-        cve_result['android_ids'] = android_ids
+                cve_result['android_ids'] = android_ids
 
-        # Various vendor references
-        references = {}
+                # Various vendor references
+                references = {}
 
-        # search for Qualcomm reference
-        match_results = QUALCOMM_REFERENCE.findall(description)
-        if match_results != []:
-            references['qualcomm'] = []
-            for match in match_results:
-                references['qualcomm'].append(match)
+                # search for Qualcomm reference
+                match_results = QUALCOMM_REFERENCE.findall(description['value'])
+                if match_results != []:
+                    references['qualcomm'] = []
+                    for match in match_results:
+                        references['qualcomm'].append(match)
 
-        # search for Broadcom reference
-        match_results = BROADCOM_REFERENCE.findall(description)
-        if match_results == []:
-            match_results = BROADCOM_REFERENCE2.findall(description)
+                # search for Broadcom reference
+                match_results = BROADCOM_REFERENCE.findall(description['value'])
+                if match_results == []:
+                    match_results = BROADCOM_REFERENCE2.findall(description['value'])
 
-        if match_results != []:
-            references['broadcom'] = []
-            # TODO: rewrite reference to standard format or keep variants?
-            for match in match_results:
-                references['broadcom'].append(match)
+                if match_results != []:
+                    references['broadcom'] = []
+                    # TODO: rewrite reference to standard format or keep variants?
+                    for match in match_results:
+                        references['broadcom'].append(match)
 
-        # search for Mediatek reference
-        match_results = MEDIATEK_REFERENCE.findall(description)
-        if match_results == []:
-            match_results = MEDIATEK_REFERENCE2.findall(description)
-        if match_results == []:
-            match_results = MEDIATEK_REFERENCE3.findall(description)
-        if match_results != []:
-            references['mediatek'] = []
-            # TODO: rewrite reference to standard format or keep variants?
-            for match in match_results:
-                references['mediatek'].append(match)
+                # search for Mediatek reference
+                match_results = MEDIATEK_REFERENCE.findall(description['value'])
+                if match_results == []:
+                    match_results = MEDIATEK_REFERENCE2.findall(description['value'])
+                if match_results == []:
+                    match_results = MEDIATEK_REFERENCE3.findall(description['value'])
+                if match_results != []:
+                    references['mediatek'] = []
+                    # TODO: rewrite reference to standard format or keep variants?
+                    for match in match_results:
+                        references['mediatek'].append(match)
 
-        # mediatek patches (TODO: should these be separate?)
-        match_results = MEDIATEK_PATCH_ID.findall(description)
-        if match_results == []:
-            match_results = MEDIATEK_PATCH_ID2.findall(description)
-        if match_results != []:
-            references['mediatek-patch'] = []
-            for match in match_results:
-                references['mediatek-patch'].append(match)
+                # mediatek patches (TODO: should these be separate?)
+                match_results = MEDIATEK_PATCH_ID.findall(description['value'])
+                if match_results == []:
+                    match_results = MEDIATEK_PATCH_ID2.findall(description['value'])
+                if match_results != []:
+                    references['mediatek-patch'] = []
+                    for match in match_results:
+                        references['mediatek-patch'].append(match)
 
-        # NVIDIA reference
-        match_results = NVIDIA_REFERENCE.findall(description)
-        if match_results != []:
-            references['nvidia'] = []
-            for match in match_results:
-                references['nvidia'].append(match)
+                # NVIDIA reference
+                match_results = NVIDIA_REFERENCE.findall(description['value'])
+                if match_results != []:
+                    references['nvidia'] = []
+                    for match in match_results:
+                        references['nvidia'].append(match)
 
-        cve_result['vendor_references'] = references
+                cve_result['vendor_references'] = references
 
-        # other CVEs
-        cve_references = []
-        match_results = CVE_REFERENCE.findall(description)
-        if match_results != []:
-            for match in match_results:
-                if match != cve_number:
-                    cve_references.append(match)
+                # other CVEs
+                cve_references = []
+                match_results = CVE_REFERENCE.findall(description['value'])
+                if match_results != []:
+                    for match in match_results:
+                        if match != cve_number:
+                            cve_references.append(match)
 
-        cve_result['cve_references'] = cve_references
-        print(cve_result)
-        print()
+                cve_result['cve_references'] = cve_references
+                print(cve_result)
+                print()
 
+            if skip_cve:
+                continue
 
 
 if __name__ == "__main__":
