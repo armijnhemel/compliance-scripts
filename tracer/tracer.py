3a996483662e94a3eca763a59cbbc7e929ceea40
Armijn Hemel <armijn@tjaldur.nl>
2024-07-14 20:29:15 +0200

    remove information that was moved to README.md

--- tracer/tracer.py
+++ tracer/tracer.py
@@ -1,59 +1,6 @@
 #!/usr/bin/env python3
 
 # Trace file processor
-#
-# Background information:
-#
-# * http://www.st.ewi.tudelft.nl/~sander/pdf/publications/TUD-SERG-2012-010.pdf
-# * http://rebels.ece.mcgill.ca/confpaper/2014/09/14/tracing-software-build-processes-to-uncover-license-compliance-inconsistencies.html
-#
-# SPDX-License-Identifier: GPL-3.0
-#
-# Copyright 2017-2024 - Armijn Hemel
-#
-# ---- USAGE ----
-#
-# Example: Linux kernel
-#
-# First trace a Linux kernel build with (for example) the following command:
-#
-# strace -o ../linux-strace -e trace=%file,%process,dup,dup2,dup3,close,pipe,tee,fchdir -y -Y -qq -f --seccomp-bpf -s 256 make
-#
-# (TODO: is 'close()' really needed?)
-#
-# and then run this script on the output.
-#
-# Alternatively, to save diskspace for the trace file (sometimes up to 50%), use:
-#
-# strace -o ../linux-strace -e trace=chdir,getcwd,link,linkat,mkdir,open,openat,rename,renameat2,sendfile,symlink,symlinkat,unlink,unlinkat,%process,dup,dup2,dup3,close,pipe,tee,fchdir -y -Y -qq -f --seccomp-bpf -s 256 make
-#
-# The following syscalls (from %file) can be ignored:
-#
-# * access
-# * chmod
-# * faccessat2
-# * fchmodat
-# * newfstatat
-# * readlink
-# * statfs
-# * utimensat
-#
-# The following syscalls (from %file) can possibly be ignored:
-#
-# * mkdir
-# * unlink
-# * unlinkat
-#
-# TODO: There are probably more calls that need to be added
-# * fcntl
-# * sendfile64
-#
-# TODO: what to do with writes to files? There are sometimes zero sized files
-# that are merely touched, but no content is written to them. Should write
-# calls such as write() also be tracked?
-#
-# Make sure there is enough disk space available, as trace files for the
-# Linux kernel tend to be quite big.
 
 import copy
 import datetime
